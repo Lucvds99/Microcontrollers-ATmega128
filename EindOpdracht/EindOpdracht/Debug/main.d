f91e8090a2c2e9fbe89d4f7fba846134b9aaea96
TeunGit <afm.lauwerijssen@student.avans.nl>
2024-03-28 11:49:23 +0100

    implemented timer and interrupt

--- EindOpdracht/EindOpdracht/Debug/main.d
+++ EindOpdracht/EindOpdracht/Debug/main.d
@@ -10,8 +10,14 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- .././include/LcdController.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ .././include/lcdController.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -35,6 +41,18 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
-.././include/LcdController.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+.././include/lcdController.h:
--- EindOpdracht/EindOpdracht/main.c
+++ EindOpdracht/EindOpdracht/main.c
@@ -1,117 +1,83 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 15/03/2024 10:43:35
+ * Author : teunl
+ */ 
+
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
-#include "include/LcdController.h"
+#include <stdio.h>
+#include "include/lcdController.h"
 
-// Definieer pinnen voor knop en LED
-#define BUTTON_PIN  (1 << PD2)  // Pin 2 op Port D
-#define LED_PIN     (1 << PB0)  // Pin 0 op Port B
+#define SECOND_ON_PRE_1024 9766
 
-// Definieer variabelen voor timing
-volatile uint16_t timer_ticks = 0;
-volatile uint16_t button_down_ticks = 0;
-volatile uint8_t morse_buffer = 0;
-volatile uint8_t morse_length = 0;
-volatile uint8_t word_flag = 0;
+void wait(int ms);
+char word_buffer;
+uint16_t morse_units = 0;
+volatile uint8_t READING_INPUT = 0;
 
-// Functie om morsecode te decoderen naar letters
-char decode_morse(uint8_t code) {
-	switch(code) {
-		case 0b01:
-		 lcd_display_text("*");
-		 return '*';   // *
-		case 0b11: return '-';   // -
-		// Voeg hier de rest van de morsecode toe
-		default: return '?';     // Onbekende code
-	}
-}
-
-// Timer interrupt service routine
-ISR(TIMER1_COMPA_vect) {
-	timer_ticks++;
-}
-
-// Externe interrupt service routine voor knop
-ISR(INT0_vect) {
-	if (bit_is_clear(PIND, PD2)) {
-		// Knop is ingedrukt
-		button_down_ticks = timer_ticks;
-		} else {
-		// Knop is losgelaten
-		uint16_t duration = timer_ticks - button_down_ticks;
-		
-		if (duration >= 7000) { // 7 seconden
-			// Leeg morsebuffer, dit geeft een nieuw woord aan
-			morse_buffer = 0;
-			morse_length = 0;
-			word_flag = 1;
-			} else if (duration >= 3000) { // 3 seconden
-			// Voeg spaties toe aan morsebuffer om nieuwe letter aan te geven
-			if (morse_length > 0) {
-				morse_buffer <<= 1;
-				morse_length++;
-			}
-			} else if (duration >= 1000) { // 1 seconde
-			// Voeg punt toe aan morsebuffer
-			morse_buffer <<= 1;
-			morse_buffer |= 1;
-			morse_length++;
-			} else { // Korter dan 1 seconde
-			// Voeg streepje toe aan morsebuffer
-			morse_buffer <<= 1;
-			morse_buffer |= 1;
-			morse_buffer <<= 1; // Add extra 0 for encoding "-"
-			morse_length++;
-		}
-	}
-	button_down_ticks = 0;
-}
-
-int main(void) {
-	lcd_init();
-	lcd_display_text("hallo ");
-	
-	
-	while(1){
-		lcd_display_text("hallo ");
+ISR(INT0_vect)
+{
+	READING_INPUT = (PIND & 1);
+	if(READING_INPUT == 0 )
+	{
+		//get morse value and use morse map to determine Letter
+		char count[16];
+		lcd_clear_display();
+		sprintf(count, "%d", morse_units); // Convert integer to string
+		lcd_display_text(count);
+		morse_units = 1;
+		PORTB = (0 << 0) ; // Toggle the LED
+	}else
+	{
+		TIFR = (1 << OCF1A ) ; // clear the CTC flag ( writing a logic one to the set flag clears it)
+		PORTB = (1 << 0) ; // Toggle the LED
 	}
 	
-	
-	// Initialiseer timer
-	TCCR1B |= (1 << WGM12);     // CTC mode
-	OCR1A = 15625;              // Vergelijkswaarde voor 1 seconde bij prescaler 1024
-	TIMSK |= (1 << OCIE1A);    // Activeer compare match interrupt
-	TCCR1B |= (1 << CS12) | (1 << CS10); // Prescaler op 1024 inschakelen
-	sei();                      // Activeer globale interrupts
+}
 
+int main(void)
+{
+	DDRB |= (1 << 0) | (1 << 1) ; // Set LED as output
+	TCCR1B |= (1 << WGM12 ); // Configure timer 1 for CTC mode
+	OCR1A = (SECOND_ON_PRE_1024); // Set CTC compare value to 1Hz at 1 MHz AVR clock , with a prescaler of 64
+	TCCR1B |= ((1 << CS10 ) | (0 << CS11 )) | (1 << CS12) ; // Start timer at Fcpu /1024
+	
 	// Initialiseer knop
-	EICRA |= (1 << ISC01);      // Externe interrupt trigger op falling edge
+	EICRA |= (1 << ISC00);      // Externe interrupt trigger op any digital change
 	EIMSK |= (1 << INT0);       // Activeer externe interrupt 0
-
-	// Initialiseer pinnen
-	DDRB |= LED_PIN;            // Zet LED pin als output
-	DDRD &= ~BUTTON_PIN;        // Zet knop pin als input
-	PORTD |= BUTTON_PIN;        // Activeer pull-up weerstand op knop pin
-
-	while (1) {
-		// Decodeer morsebuffer naar letter
-		if (morse_length > 0 && !word_flag) {
-			char letter = decode_morse(morse_buffer);
-			if (letter != '?') {
-				// Doe iets met de gedecodeerde letter, zoals LED aan/uit zetten
-				if (letter == '*') {
-					PORTB ^= LED_PIN; // Toggle LED
-				}
-				// Optioneel: voeg ruisonderdrukking toe
-				morse_buffer = 0; // Reset morsebuffer na verwerking
-				morse_length = 0;
+	sei();
+	
+	
+	
+	
+	lcd_init();
+	lcd_clear_display();
+	for (;;)
+	{
+		if (READING_INPUT){
+			if ( TIFR & (1 << OCF1A ))
+			{
+				morse_units ++;	
+				PORTB ^= (1 << 0) ; // Toggle the LED
+				TIFR = (1 << OCF1A ) ; // clear the CTC flag ( writing a logic one to the set flag clears it)	
 			}
+			
 		}
-		// Reset woordvlag na verwerking van woord
-		if (word_flag) {
-			word_flag = 0;
-		}
-		// Voeg eventueel een kleine vertraging toe om de CPU te ontlasten
+		
 	}
-
-	return 0;
+	
+	
+	
 }
+void wait( int ms )
+{
+	for (int tms=0; tms<ms; tms++)
+	{
+		_delay_ms( 1 );            // library function (max 30 ms at 8MHz)
+	}
+}
+
